bdcca8742f484e6b9de2fd291a779726b5c5ae31
MennaTullahTaha <menna.tullah.magdy731@gmail.com>
2020-12-07 20:08:00 +0200

    Added users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum:102}, format: {with: VALID_EMAIL_REGEX }
     has_many :articles
+    has_secure_password
+
 end 
